c89dfcc0c1918966b75f81b6ba6c91f0f03c3d98
Dmitry Kuchukov <kudim84@yandex.ru>
2020-05-10 02:42:56 +0300

    добавляет моки точки маршрута

---
+++ src/mock/point-route.js
@@ -0,0 +1,22 @@
+const eventType = new Map([
+  [`Taxi`, `./img/icons/taxi.png`],
+  [`Flight`, `./img/icons/flight.png`],
+  [`Drive`, `./img/icons/drive.png`],
+  [`Ship`, `./img/icons/ship.png`],
+  [`Bus`, `./img/icons/bus.png`],
+  [`Check`, `./img/icons/check-in.png`],
+  [`Restaurant`, `./img/icons/restaurant.png`],
+  [`Sightseeing`, `./img/icons/sightseeing.png`],
+  [`Train`, `./img/icons/train.png`],
+  [`Transport`, `./img/icons/transport.png`],
+  [`Trip`, `./img/icons/sightseeing.png`]]
+);
+
+const getRandomArrayItem = (array) => {
+  const randomIndex = getRandomIntegerNumber(0, array.length);
+  return array[randomIndex];
+};
+
+const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(max * Math.random());
+};
