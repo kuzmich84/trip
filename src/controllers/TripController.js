9bac242eff65e9d4e1dc87a4abe176ce1f6bee5c
Dmitry Kuchukov <kudim84@yandex.ru>
2020-05-29 22:42:36 +0300

    перенесет логику отрисовки точек маршрута

--- src/controllers/TripController.js
+++ src/controllers/TripController.js
@@ -1,3 +1,35 @@
-export default class TripController {
+import PointRouteComponent from "../components/point-route";
+import RouteEditComponent from "../components/route-edit";
+import {render, RenderPosition} from "../utils/render";
 
+export default class TripController {
+  const renderRoute = (point, currentDay) => {
+    const routePointComponent = new PointRouteComponent(point);
+    const routeEditComponent = new RouteEditComponent(point);
+
+    const onEscKeyDown = (evt) => {
+      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+      if (isEscKey) {
+        replaceEditToRoute();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+    const eventsList = currentDay.querySelector(`.trip-events__list`);
+    const replaceEditToRoute = () => eventsList.replaceChild(routePointComponent.getElement(), routeEditComponent.getElement());
+    const replaceRouteToEdit = () => eventsList.replaceChild(routeEditComponent.getElement(), routePointComponent.getElement());
+
+    const editButton = routePointComponent.getElement().querySelector(`.event__rollup-btn`);
+
+    editButton.addEventListener(`click`, () => {
+      replaceRouteToEdit();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    routeEditComponent.setSubmitHandler(replaceEditToRoute);
+    routeEditComponent.setCloseButtonClickHandler(replaceEditToRoute);
+
+
+    render(eventsList, routePointComponent.getElement(), RenderPosition.BEFOREEND);
+  };
 }
--- src/main.js
+++ src/main.js
@@ -16,35 +16,7 @@ import {NoRoutesComponent} from './components/no-routes.js';
 const COUNTCARD = 3;
 const COUNTPOINTROUTE = 5;
 
-const renderRoute = (point, currentDay) => {
-  const routePointComponent = new PointRouteComponent(point);
-  const routeEditComponent = new RouteEditComponent(point);
 
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      replaceEditToRoute();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-  const eventsList = currentDay.querySelector(`.trip-events__list`);
-  const replaceEditToRoute = () => eventsList.replaceChild(routePointComponent.getElement(), routeEditComponent.getElement());
-  const replaceRouteToEdit = () => eventsList.replaceChild(routeEditComponent.getElement(), routePointComponent.getElement());
-
-  const editButton = routePointComponent.getElement().querySelector(`.event__rollup-btn`);
-
-  editButton.addEventListener(`click`, () => {
-    replaceRouteToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  routeEditComponent.setSubmitHandler(replaceEditToRoute);
-  routeEditComponent.setCloseButtonClickHandler(replaceEditToRoute);
-
-
-  render(eventsList, routePointComponent.getElement(), RenderPosition.BEFOREEND);
-};
 
 
 const points = generatePoints(COUNTPOINTROUTE);
